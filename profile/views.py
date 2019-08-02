b39e281e6893e904efb7d9196ab879180955067d
rvllfil <rullfhiel@gmail.com>
2019-08-03 02:08:52 +0700

    mengubah tampilan app profile

--- profile/views.py
+++ profile/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from datetime import datetime, date
-# Enter your name here
-mhs_name = 'Rully Fildansyah'  # TODO Implement this
+
+mhs_name = 'Rully Fildansyah'  
 curr_year = int(datetime.now().strftime("%Y"))
-birth_date = date(1998, 7, 11)  # TODO Implement this, format (Year, Month, Date)
-departement = 'Teknik Informatika' 										# TODO Implement this
-city = 'Sukabumi' 														# TODO Implement this
-interest = 'Web, IOT, dan Bahasa Python (Pythonista).' 					# TODO Implement this
-instagram = 'https://www.instagram.com/rvllfil/'						# TODO Implement this
-github = 'https://github.com/rvllfil'									# TODO Implement this
-linkedin = 'https://www.linkedin.com/in/rully-fildansyah-7a85b0168/'  	# TODO Implement this
-twitter = 'https://twitter.com//rvllfil'								# TODO Implement this
+birth_date = date(1998, 7, 11)  
+departement = 'Teknik Informatika' 										
+city = 'Sukabumi' 
+phone = '+62 898 6431 126'
+email = 'rullfhiel@gmail.com'														
+interest = 'Web developer, IOT & Pythonista' 					
+instagram = 'https://www.instagram.com/rvllfil/'						
+github = 'https://github.com/rvllfil'									
+linkedin = 'https://www.linkedin.com/in/rully-fildansyah-7a85b0168/'  	
+twitter = 'https://twitter.com//rvllfil'								
 
 # Create your views here.
-
 
 def index(request):
     response = {
@@ -22,6 +23,8 @@ def index(request):
         'birth_date': birth_date,
         'departement': departement,
         'city': city,
+        'phone': phone,
+        'email': email,
         'interest': interest,
         'instagram': instagram,
         'github': github,
