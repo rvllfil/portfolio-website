e2469619fca84bff6508fa8cff72b8b1f024f146
rvllfil <rullfhiel@gmail.com>
2019-08-03 10:46:31 +0700

    membuat tampilan app resume

--- resume/views.py
+++ resume/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+	
+mhs_name = 'Rully Fildansyah' 
 
 def index(request):
 	response = {
+	'name': mhs_name,
 
 	}
 	return render(request, 'resume.html', response)
